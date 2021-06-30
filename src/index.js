24d5c0acd714e74e73a758d32e375a8cfd97de00
jschles-sooryen <john.schlesinger@sooryen.com>
2021-06-30 15:49:55 -0400

    Basic redux set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
 
 import App from './App';
+import store from './store';
 import theme from './styles/theme';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 );
