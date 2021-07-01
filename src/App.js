ecdd92a4b5644a0beac715daadcd0a3e64059f59
jschles-sooryen <john.schlesinger@sooryen.com>
2021-07-01 15:52:27 -0400

    Finish Header component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Layout>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            Notes App
-          </Route>
-        </Switch>
-      </Router>
-    </Layout>
+    <Router>
+      <Layout>
+          <Switch>
+            <Route exact path="/">
+              Notes
+            </Route>
+            <Route exact path="/create">
+              Create Note
+            </Route>
+          </Switch>
+      </Layout>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,26 @@ import withStyles from '@material-ui/core/styles/withStyles';
 const styles = (theme) => ({
   root: {
     margin: '0 auto',
+    borderBottom: `1px solid ${theme.palette.primary.main}`
   },
   flexContainer: {
     display: 'flex',
     maxWidth: 1440,
     justifyContent: 'space-between',
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+    '& h1': {
+      fontSize: 22,
+    },
+  },
+  linkContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    '& a': {
+      display: 'block',
+    },
+    '& > :first-child': {
+      marginRight: theme.spacing(1),
+    },
   },
 });
 
@@ -18,7 +33,12 @@ const Header = (props) => {
   return (
     <div className={classes.root}>
       <div className={classes.flexContainer}>
+        <h1>React Notes App V1</h1>
 
+        <div className={classes.linkContainer}>
+          <Link to="/">All Notes</Link>
+          <Link to="/create">Create Note</Link>
+        </div>
       </div>
     </div>
   );
