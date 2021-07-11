6d95de22d2b954da9f62d177c653fa2bd53b02c5
jschles-sooryen <john.schlesinger@sooryen.com>
2021-07-11 17:33:06 -0400

    Finish DB schema

--- server/database.js
+++ server/database.js
@@ -13,26 +13,30 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
   } else {
     console.log('Connected to the SQLite database.');
 
-    // Commenting out for now until schema is established
-
-    // db.run(`CREATE TABLE user (
-    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    //         name text,
-    //         email text UNIQUE,
-    //         password text,
-    //         CONSTRAINT email_unique UNIQUE (email)
-    //         )`,
-    // (error) => {
-    //   if (error) {
-    //     console.error('Error connecting to SQLite database: ', error);
-    //     // Table already created
-    //   } else {
-    //     // Table just created, creating some rows
-    //     // const insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)';
-    //     // db.run(insert, ['admin', 'admin@example.com', md5('admin123456')]);
-    //     // db.run(insert, ['user', 'user@example.com', md5('user123456')]);
-    //   }
-    // });
+    db.run(`
+      CREATE TABLE folders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name text
+      );
+      CREATE TABLE notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name text,
+        description text,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        folder_id INTEGER,
+        FOREIGN_KEY(folder_id) REFERENCES folders(id)
+      );
+    `,
+    (error) => {
+      if (error) {
+        console.error('Tables already created ', error);
+        // Table already created
+      } else {
+        // Table just created, creating some rows
+        const insert = 'INSERT INTO folders (name) VALUES (?)';
+        db.run(insert, ['Notes']);
+      }
+    });
   }
 });
 
