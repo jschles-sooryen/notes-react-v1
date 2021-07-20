4a6fac63212db3109906dd9d2b010d86956af08a
jschles-sooryen <john.schlesinger@sooryen.com>
2021-07-20 17:28:58 -0400

    Remove console.log

--- src/store/reducers/loadingReducer.js
+++ src/store/reducers/loadingReducer.js
@@ -5,7 +5,6 @@ const initialState = false;
 function loadingReducer(state = initialState, action) {
   switch (action.type) {
     case LOADING:
-      console.log('loading change');
       return !state;
     default:
       return state;
