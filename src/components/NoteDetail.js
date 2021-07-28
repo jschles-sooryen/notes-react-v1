14181c3ccedf63156616cf2f51588ea819f1b889
jschles-sooryen <john.schlesinger@sooryen.com>
2021-07-28 17:12:51 -0400

    Close new note form if user clicks off NoteDetail with no value entered

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useForm, Controller } from 'react-hook-form';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { toggleCreateNote } from '../store/actions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,22 +25,31 @@ const NoteDetail = ({ isNew }) => {
   const description = notes[selected]?.description;
   const updatedAt = notes[selected]?.updated_at;
   const classes = useStyles();
-  const { control } = useForm({
+  const { control, getValues } = useForm({
     defaultValues: {
       description: isNew ? '' : description,
     },
   });
 
+  const handleOnBlur = () => {
+    const formValues = getValues();
+
+    if (isNew && !formValues.description.trim()) {
+      dispatch(toggleCreateNote());
+    }
+  };
+
   return (
     <div className={classes.root}>
       <Controller
         control={control}
-        name="name"
+        name="description"
         render={({ field }) => (
           <TextField
             {...field}
             autoFocus
             multiline
+            onBlur={handleOnBlur}
             variant="outlined"
             classes={{ root: classes.textarea }}
             InputProps={{
