302d72b03cb1b0e49f536df3dcddb55c881db80f
jschles-sooryen <john.schlesinger@sooryen.com>
2021-11-17 18:02:31 -0500

    Revert "update callback uri"
    This reverts commit eaa3a77d9a396ab7acb422de4be7037472fb97a4.

--- server/functions/auth/index.ts
+++ server/functions/auth/index.ts
@@ -32,7 +32,7 @@ import { sign } from 'jsonwebtoken';
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID as string,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  callbackURL: 'https://localhost:3000',
+  callbackURL: `${process.env.REACT_APP_API_SERVER}/auth/callback`,
 },
   (accessToken, refreshToken, profile, done) => {
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
